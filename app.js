c3d06a6a3db0f158fc35e3ceec25089d4f3ecf36
Ábrahám Miklós <mikcsek2@gmail.com>
2023-01-14 15:05:06 +0100

    rendelések kitöltve.

---
+++ app.js
@@ -0,0 +1,54 @@
+const   result = document.getElementById("result");
+
+const orders = [
+
+    {
+        id: 1,
+        pizza: "Hawaii",
+        extra: "Kukrica",
+        ital: "Pepsi zero",
+        ar: 1500,
+        kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+    {
+        id: 2,
+        pizza: "Songoku",
+        extra: "Ketchup",
+        ital: "Pepsi",
+        ar: 2000,
+        kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+    {
+        id: 3,
+        pizza: "Magyaros",
+        extra: "Sajt",
+        ital: "Sprite",
+        ar: 1800,
+        kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+    {
+        id: 4,
+        pizza: "Quatro",
+        extra: "Sajt",
+        ital: "Coca Cola",
+        ar: 2100,
+        kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+    {
+        id: 5,
+        pizza: "Diablo",
+        extra: "Csípős szósz",
+        ital: "Víz",
+        ar: 2000,
+        kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    },
+    {
+        id: 6,
+        pizza: "Tenger gyümölcsei",
+        extra: "Csípős",
+        ital: "Pepsi zero",
+        ar: 2000,
+        kep: "https://images.pexels.com/photos/6937416/pexels-photo-6937416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    }
+]
+
